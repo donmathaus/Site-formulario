146f4ce73d79ba942e081e0c3139dc1751b0de53
Mathäus Hamermüller <don.canoas@hotmail.com>
2023-02-10 20:08:20 -0300

    Implementação do código base
    implementado alem do código base:
    formulario com autocomplete, estilização com CSS
    e "php".

---
+++ src/php/form.php
@@ -0,0 +1,3 @@
+OS DADOS FORAM ENVIADOS!
+
+Obs: Esse site é um teste front end, a parte de PHP não foi concluída. Os dados nesse método não estão seguros, pois não foi utilizado o protocolo de segurança https.
